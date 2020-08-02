6f476fa4fa05a845096c1577d706fe4781a0de8a
Ernest <teluk.ernest@gmail.com>
2020-08-02 11:37:15 +0200

    add loading

---
+++ src/Components/Loading/Loading.styled.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const LoadingWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.Colors.white};
+  opacity: 0.2;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+export const TextWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TextBox = styled.p`
+  width: 100%;
+  height: auto;
+  font-family: ${({ theme }) => theme.FontFamily.century};
+  font-size: ${({ theme }) => theme.FontSize.xxl};
+  font-weight: ${({ theme }) => theme.FontWeight.regular};
+  color: ${({ theme }) => theme.Colors.grey};
+  text-align: center;
+`;
