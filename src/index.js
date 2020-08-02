7c0643ef92b54dd7e26ce822c4f19be3bf1c4985
Ernest <teluk.ernest@gmail.com>
2020-08-02 14:09:43 +0200

    delete redux devtools code

--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 
 import App from 'Components/App/App';
 import History from 'History.js';
 import Reducers from 'State/Reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  Reducers,
-  composeEnhancers(applyMiddleware(reduxThunk))
-);
+const store = createStore(Reducers, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <React.StrictMode>
